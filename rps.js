f5f88b3f1877f9868671d7831d4ae878a8971716
Adam Heckler <aheckler@users.noreply.github.com>
2014-01-29 15:10:12 -0500

    Revert to working version

--- rps.js
+++ rps.js
@@ -5,81 +5,74 @@ var compChoice = null;
 var userWins = 0;
 var compWins = 0;
 
-// Returns rock, paper, or scissors randomly
-function getCompChoice() {
-    var randomInt = Math.floor(Math.random() * 3);
-    if (randomInt === 0) {
-            return "rock";
-        } else if (randomInt == 1) {
-            return "paper";
-        } else {
-            return "scissors";
-    }
-}
+// Main while loop
+while ( keepPlaying == true ) {
 
-// If user wins
-function youWin() {
-    userWins++;
-    document.getElementById("user-score").innerHTML=userWins;
-    alert("You win! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
-    return true;
-}
+	// Returns rock, paper, or scissors randomly
+	function getCompChoice () {
+		var randomInt = Math.floor(Math.random() * 3);
+		if (randomInt == 0) {
+			return "rock";
+		} else if (randomInt == 1) {
+			return "paper";
+		} else {
+			return "scissors";
+		}
+	}
 
-// If user loses
-function youLose() {
-    compWins++;
-    document.getElementById("comp-score").innerHTML=compWins;
-    alert("You lose! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
-    return true;
-}
+	// Set compChoice
+	var compChoice = getCompChoice();
 
-// Tie
-function tie() {
-    alert("It's a tie! The computer also chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
-    return true;
-}
+	// Set userChoice
+	var userChoice = prompt("Choose rock, paper, or scissors:");
 
-// Main game function
-function rps() {
-        
-    // Loop game if user wants
-    while (keepPlaying === true) {
+	// If user wins
+	function youWin () {
+		userWins++;
+		alert("You win! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
+		return true;
+	}
 
-        // Set compChoice
-        var compChoice = getCompChoice();
+	// If user loses
+	function youLose () {
+		compWins++;
+		alert("You lose! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
+		return true;
+	}
 
-        // Set userChoice
-        var userChoice = prompt("Choose rock, paper, or scissors:");
+	// Tie
+	function tie () {
+		alert("It's a tie! The computer also chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
+		return true;
+	}
 
-        // Find the winner!
-        if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
-            if (userChoice == compChoice) {
-                tie();
-            } else if (userChoice == "rock") {
-                if (compChoice == "paper") {
-                    youLose();
-                } else {
-                    youWin();
-                }
-            } else if (userChoice == "paper") {
-                if (compChoice == "rock") {
-                    youWin();
-                } else {
-                    youLose();
-                }
-            } else {
-                if (compChoice == "rock") {
-                    youLose();
-                } else {
-                    youWin();
-                }
-            }
-        } else {
-            // User entry non-valid
-            alert("Choose either rock, paper, or scissors.");
-            return false;
-        }
+	if ( userChoice == "rock" || userChoice == "paper" || userChoice == "scissors" ) {
+		if (userChoice == compChoice) {
+			tie();
+		}
+		else if (userChoice == "rock") {
+			if (compChoice == "paper") {
+				youLose();
+			} else {
+				youWin();
+			}
+		} else if (userChoice == "paper") {
+			if (compChoice == "rock") {
+				youWin();
+			} else {
+				youLose();
+			}
+		} else {
+			if (compChoice == "rock") {
+				youLose();
+			} else {
+				youWin();
+			}
+		}
+	} else {
+		alert("Choose either rock, paper, or scissors.");
+		return false;
+	}
 
-        keepPlaying = confirm("Do you want to keep playing?");
-    }
+	keepPlaying = confirm("Do you want to keep playing?");
 }
