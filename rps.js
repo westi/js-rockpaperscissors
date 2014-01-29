3743bcf3e22015f456e878f60be9adc4f193bb4a
Adam Heckler <aheckler@users.noreply.github.com>
2014-01-29 09:27:18 -0500

    Create function to get computer choice

--- rps.js
+++ rps.js
@@ -3,3 +3,14 @@
 // Initialize variables
 var userChoice = null;
 var compChoice = null;
+
+function getCompChoice () {
+	// Returns a random integer between min and max
+	if (return Math.floor(Math.random() * 3) == 0) {
+		return "rock";
+	} else if (return Math.floor(Math.random() * 3) == 1) {
+		return "paper";
+	} else {
+		return "scissors";
+	}
+}
