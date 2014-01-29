16f58c1a38174eb31fc8be0296fb20a8a2b83bec
Adam Heckler <aheckler@users.noreply.github.com>
2014-01-29 12:54:32 -0500

    Everything in user-controlled while loop

--- rps.js
+++ rps.js
@@ -1,74 +1,78 @@
-// Rock Paper Scissors
-
 // Initialize variables
+var keepPlaying = true;
 var userChoice = null;
 var compChoice = null;
 var userWins = 0;
 var compWins = 0;
 
-// Returns rock, paper, or scissors randomly
-function getCompChoice () {
-	var randomInt = Math.floor(Math.random() * 3);
-	if (randomInt == 0) {
-		return "rock";
-	} else if (randomInt == 1) {
-		return "paper";
+// Main while loop
+while ( keepPlaying == true ) {
+
+	// Returns rock, paper, or scissors randomly
+	function getCompChoice () {
+		var randomInt = Math.floor(Math.random() * 3);
+		if (randomInt == 0) {
+			return "rock";
+		} else if (randomInt == 1) {
+			return "paper";
+		} else {
+			return "scissors";
+		}
+	}
+
+	// Set compChoice
+	var compChoice = getCompChoice();
+
+	// Set userChoice
+	var userChoice = prompt("Choose rock, paper, or scissors:");
+
+	// If user wins
+	function youWin () {
+		userWins++;
+		alert("You win! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
+		return true;
+	}
+
+	// If user loses
+	function youLose () {
+		compWins++;
+		alert("You lose! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
+		return true;
+	}
+
+	// Tie
+	function tie () {
+		alert("It's a tie! The computer also chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
+		return true;
+	}
+
+	if ( userChoice == "rock" || userChoice == "paper" || userChoice == "scissors" ) {
+		if (userChoice == compChoice) {
+			tie();
+		}
+		else if (userChoice == "rock") {
+			if (compChoice == "paper") {
+				youLose();
+			} else {
+				youWin();
+			}
+		} else if (userChoice == "paper") {
+			if (compChoice == "rock") {
+				youWin();
+			} else {
+				youLose();
+			}
+		} else {
+			if (compChoice == "rock") {
+				youLose();
+			} else {
+				youWin();
+			}
+		}
 	} else {
-		return "scissors";
+		alert("Choose either rock, paper, or scissors.");
+		return false;
 	}
-}
 
-// Set compChoice
-var compChoice = getCompChoice();
-
-// Set userChoice
-var userChoice = prompt("Choose rock, paper, or scissors:");
-
-// If user wins
-function youWin () {
-	userWins++;
-	alert("You win! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
-	return true;
-}
-
-// If user loses
-function youLose () {
-	compWins++;
-	alert("You lose! The computer chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
-	return true;
-}
-
-// Tie
-function tie () {
-	alert("It's a tie! The computer also chose " + compChoice + ". The score is " + userWins + " to " + compWins + ".");
-	return true;
-}
-
-// Main if statement
-if ( userChoice == "rock" || userChoice == "paper" || userChoice == "scissors" ) {
-	if (userChoice == compChoice) {
-		tie();
-	}
-	else if (userChoice == "rock") {
-		if (compChoice == "paper") {
-			youLose();
-		} else {
-			youWin();
-		}
-	} else if (userChoice == "paper") {
-		if (compChoice == "rock") {
-			youWin();
-		} else {
-			youLose();
-		}
-	} else {
-		if (compChoice == "rock") {
-			youLose();
-		} else {
-			youWin();
-		}
-	}
-} else {
-	alert("Choose either rock, paper, or scissors.");
-	return false;
+	keepPlaying = confirm("Do you want to keep playing?");
 }
